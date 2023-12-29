db0e31c0c479d04d21d1d2531e8f6515fd26e965
Savchenko Alexander <asavchenko@datatech.ru>
2023-12-29 13:40:07 +0400

    ref(a-Collapse) forwardRef

--- ui/collapse.tsx
+++ ui/collapse.tsx
@@ -1,53 +1,52 @@
 import { a, useSpring } from '@react-spring/web'
 
-import React, { CSSProperties } from 'react'
-import useMeasure from 'react-use-measure'
+import React, { CSSProperties, ForwardedRef, forwardRef } from 'react'
 
 import { Position, c } from '~/utils/core'
+import { useMeasure } from '~/utils/hooks'
+import { setRefs } from '~/utils/react'
 
-export interface Props {
-  children: React.ReactNode
+export interface Props extends React.HTMLAttributes<HTMLDivElement> {
   isExpanded: boolean
   from?: (CSSProperties & Partial<Position>) | undefined
   to?: (CSSProperties & Partial<Position>) | undefined
   duration?: number | undefined
-  rootProps?: React.HTMLAttributes<HTMLDivElement> | undefined
   containerProps?: React.HTMLAttributes<HTMLDivElement> | undefined
-  className?: string
 }
 
 const displayName = 'a-Collapse'
 
-export default function Component(props: Props): JSX.Element {
-  const [ref, measure] = useMeasure()
-  const viewHeight = measure.height || 'auto'
+function Component(props: Props, ref: ForwardedRef<HTMLDivElement>): JSX.Element {
+  const { containerProps, duration, to, from, isExpanded, children, ...divProps } = props
+
+  const [measureRef, measure] = useMeasure()
+  const height = measure.height || 'auto'
 
   const springProps = useSpring({
-    config: { duration: props.duration ?? 200 },
-    from: { height: viewHeight, ...props.from },
-    to: {
-      height: props.isExpanded ? viewHeight : 0,
-      ...props.to,
-    },
+    from,
+    to: { height: isExpanded ? height : 0, ...to },
+    config: { duration: duration ?? 200 },
   })
 
   return (
     <a.div
-      {...props.rootProps}
-      style={{ overflow: 'hidden', width: '100%', ...props.rootProps?.style, ...springProps }}
+      {...divProps}
+      style={{ overflow: 'hidden', width: '100%', ...divProps.style, ...springProps }}
+      ref={ref}
       className={c(
+        divProps.className,
         displayName,
-        props.className,
-        props.rootProps?.className,
         props.isExpanded && '--expanded',
         !props.isExpanded && '--collapsed'
       )}
     >
-      <a.div {...props.containerProps} ref={ref}>
-        {props.children}
-      </a.div>
+      <div {...containerProps} className={c('container')} ref={setRefs(measureRef)}>
+        {children}
+      </div>
     </a.div>
   )
 }
 
-Component.displayName = displayName
+const ForwardRef = forwardRef(Component)
+ForwardRef.displayName = displayName
+export default ForwardRef
